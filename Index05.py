4b876460c18f156716e978ac524d02f2b5c0bb69
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-19 06:12:22 -0700

    05

--- Index05.py
+++ Index05.py
@@ -5,5 +5,26 @@ def main(s):
         s(str): parameter
     Returns:
         int: answer
-    """
-    return
+    """ 
+    if s[0].isdigit()==1:
+        x1=1 
+    else:
+        x1=0 
+    if s[1].isdigit()==1:
+        x2=1 
+    else:
+        x2=0 
+    if s[2].isdigit()==1:
+        x3=1
+    else:
+        x3=0
+    if s[3].isdigit()==1:
+        x4=1
+    else:
+        x4=0
+    if s[4].isdigit()==1:
+        x5=1
+    else:
+        x5=0
+    return x1+x2+x3+x4+x5
+print(main('32x3z'))

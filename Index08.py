9e91250cce862ac11a31e9a90b65c6319ccac3f4
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-22 01:03:33 -0700

    08

--- Index08.py
+++ Index08.py
@@ -5,6 +5,14 @@ def main(s):
         s(str): parameter
     Returns:
         int: answer
-    """
-    return
-        
+    """ 
+    if s[0]=="*": 
+        x=0
+    if s[1]=="*": 
+        x=1
+    if s[2]=="*": 
+        x=2 
+    if s[3]=="*": 
+        x=3 
+    return s[0]=="*" and x or s[1]=="*" and x or s[2]=="*" and x or s[3]=="*" and x 
+print(main('good'))        

5523f710bfaab73af80e3adfd81724f00eebfd5b
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-22 01:55:49 -0700

    010:

--- Index10.py
+++ Index10.py
@@ -5,5 +5,7 @@ def main(s):
         s(str): parameter
     Returns:
         int: answer
-    """
-    return
+    """ 
+
+    return int(s[0])+int(s[1])+int(s[2])+int(s[3])+int(s[4]) 
+print(main('12332'))
